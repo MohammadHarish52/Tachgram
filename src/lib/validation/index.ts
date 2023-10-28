5abe45356ad94ca1a51d306e0f00a8797f20545e
tachyonok <tachyonparticle123@gmail.com>
2023-10-28 19:30:32 +0530

    Sign-in and new user

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,5 +1,18 @@
 import * as z from "zod";
 
 export const SignupValidation = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: "Too  short",
+    })
+    .max(45),
   username: z.string().min(2).max(50),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, {
+      message: "Too short bitch",
+    })
+    .max(16),
 });
